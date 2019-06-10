caf40499f7a253abf22709ec188f6bfeec9b109f
bgkoussa <51185541+bgkoussa@users.noreply.github.com>
2019-06-10 17:17:22 -0400

    update setup.py exclusions

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/equinoxfitness/easy-icm-runner/",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=[
           'requests',
       ],
