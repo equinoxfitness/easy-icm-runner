f30324d007c7aab44218af447404abac150555d6
elliott <elliottcordo@gmail.com>
2019-07-31 08:41:17 -0400

    started unit tests

---
+++ tests/test_icm_runner.py
@@ -0,0 +1,82 @@
+from unittest import TestCase
+from unittest.mock import patch
+from icm_runner import Runner
+
+@patch('icm_runner.requests.post')
+class TestRunner(TestCase):
+    """
+
+    """
+
+    def test_get_token(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        mock_post.return_value.json.return_value = {'token': 'blah'}
+        Runner().get_token('', '')
+
+    def test_build_header(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        fixture_header = {'authorization': 'bearer ', 'model': 'fake_model', 'content-type': 'application/json'}
+        header = Runner().build_header('fake_model')
+        self.assertEqual(fixture_header, header)
+
+    def test_get_process_id(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        pass
+
+    def test_run_process_by_id(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        pass
+
+    def test_run_process_by_name(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        pass
+
+    def test_get_live_activity_status(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        pass
+
+    def test_get_all_completed_activities(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        pass
+
+    def test_get_completed_activity_status(self, mock_post):
+        """
+
+        :param mock_post:
+        :return:
+        """
+        pass
+
+
+if __name__ == '__main__':
+    import unittest as ut
+    ut.main()
+
