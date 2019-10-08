60ae1f6978dab08e8760e83cb88b60c179489f96
Bachir El Koussa <bachir.elkoussa@equinox.com>
2019-10-08 16:08:13 -0400

    Added 2 more tests

--- tests/test_icm_runner.py
+++ tests/test_icm_runner.py
@@ -70,7 +70,12 @@ class TestRunner(TestCase):
         :param mock_post:
         :return:
         """
-        pass
+        mock_req.post.return_value.json.return_value = \
+            {'completedactivities': 'api/v1/completedactivities/1',
+              'liveactivities': 'api/v1/liveactivities/1'}
+        mock_req.post.return_value.status_code = 200
+        activity_id = Runner().run_process_by_id('test', '1')
+        self.assertEqual(int(activity_id), 1)
 
     def test_run_process_by_name(self, mock_req):
         """
@@ -86,7 +91,35 @@ class TestRunner(TestCase):
         :param mock_post:
         :return:
         """
-        pass
+        mock_req.get.return_value.json.return_value = \
+            [{'progressId': 1,
+              'userId': 'test',
+              'type': 'Calculation',
+              'status': 'Running',
+              'time': '2019-10-08T19:54:23.76Z',
+              'percent': 99,
+              'description': 'TEST 007 Get Record Counts Sorted by Date Desc',
+              'hasDescription': True,
+              'expiresAt': '2019-10-08T20:04:57.123Z',
+              'isCancellable': True,
+              'isInitialization': False,
+              'computationId': 1},
+             {'progressId': 1,
+              'userId': 'test',
+              'type': 'Scheduler',
+              'status': 'Running',
+              'time': '2019-10-08T19:54:23.51Z',
+              'percent': 0,
+              'description': 'Executing scheduler item...',
+              'hasDescription': True,
+              'expiresAt': '2019-10-08T20:04:56.95Z',
+              'isCancellable': True,
+              'isInitialization': False,
+              'computationId': -1}, ]
+        mock_req.get.return_value.status_code = 200
+        mock_res = {'message': 'Running', 'value': 99}
+        res = Runner().get_live_activity_status('test', '1')
+        self.assertEqual(mock_res, res)
 
     def test_get_all_completed_activities(self, mock_req):
         """
@@ -104,6 +137,13 @@ class TestRunner(TestCase):
         """
         pass
 
+    def test_monitor_activity(self, mock_req):
+        """"
+
+        :param mock_post:
+        :return:
+        """
+        pass
 
 if __name__ == '__main__':
     import unittest as ut
