13f195889a4e5313e2e0d8dffcd771984c9559ea
Bachir El Koussa <bachir.elkoussa@equinox.com>
2019-10-09 20:37:24 -0400

    Added tests for all functions except exec_runner and changed the way monitor works

--- tests/test_icm_runner.py
+++ tests/test_icm_runner.py
@@ -6,13 +6,12 @@ from icm_runner import Runner
 @patch('icm_runner.requests')
 class TestRunner(TestCase):
     """
-
+    Test cases for icm_runner.py
     """
 
     def test_get_token(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
         mock_req.post.return_value.json.return_value = {'token': 'blah'}
@@ -21,8 +20,7 @@ class TestRunner(TestCase):
 
     def test_build_header(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
         fixture_header = {'authorization': 'bearer ', 'model': 'fake_model', 'content-type': 'application/json'}
@@ -31,8 +29,7 @@ class TestRunner(TestCase):
 
     def test_get_process_id(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
         mock_req.get.return_value.json.return_value = \
@@ -63,11 +60,9 @@ class TestRunner(TestCase):
         activity_id = Runner().get_process_id('test', 'test1')
         self.assertEqual(int(activity_id), 1)
 
-
     def test_run_process_by_id(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
         mock_req.post.return_value.json.return_value = \
@@ -79,16 +74,44 @@ class TestRunner(TestCase):
 
     def test_run_process_by_name(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
-        pass
+        mock_req.post.return_value.json.return_value = \
+            {'completedactivities': 'api/v1/completedactivities/1',
+              'liveactivities': 'api/v1/liveactivities/1'}
+        mock_req.post.return_value.status_code = 200
+        mock_req.get.return_value.json.return_value = \
+            [{'activation': 'Enabled',
+              'childScheduleItems': [],
+              'id': 1,
+              'lastRun': '2019-02-01T12:10:33.757Z',
+              'lastRunStatus': 'Success',
+              'name': 'test1',
+              'nextRun': '0001-01-01T00:00:00',
+              'order': 1,
+              'scheduleItemType': 'Folder',
+              'settings': {'emailOnFailure': True,
+                           'emailOnSuccess': True,
+                           'enableRetries': False,
+                           'externalToolTimeout': 0,
+                           'failEmails': ['test@test.com'],
+                           'isGlobal': False,
+                           'overrideChildSettings': False,
+                           'scheduleItemId': 1,
+                           'schedulerSettingsId': 7,
+                           'stopOnFailure': True,
+                           'stopToolOnTimeout': False,
+                           'successEmails': ['test@test.com'],
+                           'version': {'rowVersion': 1}},
+              'version': {'rowVersion': 1}}, ]
+        mock_req.get.return_value.status_code = 200
+        activity_id = Runner().run_process_by_name('test', 'test1')
+        self.assertEqual(int(activity_id), 1)
 
     def test_get_live_activity_status(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
         mock_req.get.return_value.json.return_value = \
@@ -96,72 +119,74 @@ class TestRunner(TestCase):
               'userId': 'test',
               'type': 'Calculation',
               'status': 'Running',
-              'time': '2019-10-08T19:54:23.76Z',
-              'percent': 99,
-              'description': 'TEST 007 Get Record Counts Sorted by Date Desc',
-              'hasDescription': True,
-              'expiresAt': '2019-10-08T20:04:57.123Z',
-              'isCancellable': True,
-              'isInitialization': False,
-              'computationId': 1},
-             {'progressId': 1,
-              'userId': 'test',
-              'type': 'Scheduler',
-              'status': 'Running',
-              'time': '2019-10-08T19:54:23.51Z',
-              'percent': 0,
-              'description': 'Executing scheduler item...',
-              'hasDescription': True,
-              'expiresAt': '2019-10-08T20:04:56.95Z',
-              'isCancellable': True,
-              'isInitialization': False,
-              'computationId': -1}, ]
+              'percent': 99}, ]
         mock_req.get.return_value.status_code = 200
-        mock_res = {'message': 'Running', 'value': 99}
+        mock_res = {'message': 'Running', 'value': 99, 'IsUnitTest': False}
         res = Runner().get_live_activity_status('test', '1')
         self.assertEqual(mock_res, res)
 
     def test_get_all_completed_activities(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
         mock_req.get.return_value.json.return_value = \
             [{'progressId': 1,
-              'userId': 'test@test.com',
-              'type': 'Calculation',
               'status': 'Completed',
-              'message': 'TEST 103 Get Sessions with Missing Rates calculated successfully.',
-              'time': '2019-09-27T16:44:51.573Z',
-              'messageDetails': ''}, ]
+              'message': 'Test Successful'}, ]
         mock_req.get.return_value.status_code = 200
         mock_res = \
             [{'progressId': 1,
-              'userId': 'test@test.com',
-              'type': 'Calculation',
               'status': 'Completed',
-              'message': 'TEST 103 Get Sessions with Missing Rates calculated successfully.',
-              'time': '2019-09-27T16:44:51.573Z',
-              'messageDetails': ''}, ]
+              'message': 'Test Successful'}, ]
         res = Runner().get_all_completed_activities('test')
         self.assertEqual(mock_res, res)
 
     def test_get_completed_activity_status(self, mock_req):
         """
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
-        pass
+        mock_req.get.return_value.json.return_value = \
+            [{'progressId': 1,
+              'type': 'Calculation',
+              'status': 'Completed',
+              'message': 'Test Successful'}, ]
+        mock_req.get.return_value.status_code = 200
+        mock_res = {'message': 'Test Successful', 'value': 'Completed', 'IsUnitTest': False}
+        res = Runner().get_completed_activity_status('test', '1')
+        self.assertEqual(mock_res, res)
 
-    def test_monitor_activity(self, mock_req):
+    def test_monitor_activity_complete(self, mock_req):
         """"
-
-        :param mock_post:
+        :param mock_req:
         :return:
         """
-        pass
+        mock_req.get.return_value.status_code = 200
+        mock_req.get.return_value.json.return_value = \
+            [{'progressId': 1,
+              'status': 'Completed',
+              'message': 'Test Completed successfully',
+              'percent': 100}, ]
+        res = Runner().monitor_activity('test', '1')
+        self.assertEqual(1, res)
+
+    def test_monitor_activity_live(self, mock_req):
+        """"
+        :param mock_req:
+        :return:
+        """
+        mock_req.get.return_value.status_code = 200
+        mock_req.get.return_value.json.return_value = \
+            [{'progressId': 2,
+              'status': 'Completed',
+              'message': 'Test Completed successfully',
+              'type': 'Test',
+              'percent': 100,
+              'IsUnitTest': True}, ]
+        res = Runner().monitor_activity('test', '1')
+        self.assertEqual(1, res)
+
 
 if __name__ == '__main__':
     import unittest as ut
